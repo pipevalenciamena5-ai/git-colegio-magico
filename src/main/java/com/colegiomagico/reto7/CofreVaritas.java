6d583347e8c587a8a2a53a67b8cd9a077c2517b5
Andres Valencia <pipevalenciamena5@gmail.com>
2026-09-05 11:15:47 -0500

    Reto 8 inventario pociones andres valencia

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,7 +6,13 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int max = tamanos[0];
+        for (int i = 1; i < tamanos.length; i++) {
+            if (tamanos[i] > max) {
+                max = tamanos[i];
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
