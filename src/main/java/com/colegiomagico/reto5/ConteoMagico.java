c78f6c76d70eb92b0934dd90bd8dae9fc0943deb
Andres Valencia <pipevalenciamena5@gmail.com>
2026-09-05 11:11:55 -0500

    Reto 6 sombrero clasificatorio andres valencia

--- src/main/java/com/colegiomagico/reto5/ConteoMagico.java
+++ src/main/java/com/colegiomagico/reto5/ConteoMagico.java
@@ -6,10 +6,16 @@ package com.colegiomagico.reto5;
 public class ConteoMagico {
 
     public static int contar(int limite) {
-        return 0; // TODO: Cambia esto
+        int suma = 0;
+        int i = 1;
+        while (i <= limite) {
+            suma += i;
+            i++;
+        }
+        return suma;
     }
 
     public static void main(String[] args) {
-        System.out.println("Conteo hasta 4: " + contar(4));
+        System.out.println("Conteo hasta el limite (4): " + contar(4));
     }
 }
