5075a65f878679104ab51a6ec0fe709a2229cdea
Andres Valencia <pipevalenciamena5@gmail.com>
2026-09-05 11:16:52 -0500

    invocacion familiar andres valencia Reto 9

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -6,7 +6,13 @@ package com.colegiomagico.reto8;
 public class InventarioPociones {
 
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); // TODO: Cambia esto
+        java.util.List<String> filtrados = new java.util.ArrayList<>();
+        for (String ingrediente : ingredientes) {
+            if (ingrediente.startsWith("A")) {
+                filtrados.add(ingrediente);
+            }
+        }
+        return filtrados;
     }
 
     public static void main(String[] args) {
